a41c9fbf96869db468012ab648b6b88eff71afeb
thedudxo <isaacboon@gmail.com>
2020-09-13 11:36:21 +1200

    removed old wave music code

--- Assets/Scripts/Level/Wave.cs
+++ Assets/Scripts/Level/Wave.cs
@@ -9,9 +9,6 @@ public class Wave : MonoBehaviour {
     //Waves really dont work this way but whatever, Maybie this wave has rocket thrusters on the back of it.
 
 
-    //music
-    AudioClip waveApproachingMusic;
-    AudioClip ambientBackgroundMusic;
     [SerializeField] float waveApproachingMusicMaxDistance = 5;
     bool waveIsClose = false;
 
@@ -22,46 +19,10 @@ public class Wave : MonoBehaviour {
     void Start () {
         spawnPosition = transform.position;
         waveCurrentSpeed = waveStartSpeed;
-
-        waveApproachingMusic   = GM.audioManager.GetAudioClip("WaveApproaching");
-        waveApproachingMusic.audioSource.Play();
-        waveApproachingMusic.audioSource.volume = 0;
-        ambientBackgroundMusic = GM.audioManager.GetAudioClip("BackgroundMusic");
-
     }
 
     private void Update()
     {
-        //manage wave approach music
-        float MusicVolumeNormalised  = ambientBackgroundMusic.audioSource.volume / ambientBackgroundMusic.MaxVolume;
-        float WaveVolumeNormalised   = waveApproachingMusic  .audioSource.volume / waveApproachingMusic  .MaxVolume;
-        float waveDistanceNormalised = 
-            (FrogManager.frog.transform.position.x - transform.position.x) / waveApproachingMusicMaxDistance;
-            // distance between the frog and the wave, normalised to the approach music distance
-        //Debug.Log(waveDistanceNormalised);
-
-
-        if (waveDistanceNormalised <= 1)
-        {
-            if (!waveIsClose) //first time the wave is close
-            {
-                waveIsClose = true;
-                //ambientBackgroundMusic.audioSource.Pause();
-                waveApproachingMusic.audioSource.volume = 1;
-            }
-            else //wave has been close for a while
-            {
-
-            }
-        }
-        else if (waveIsClose) //wave isnt close anymore
-        {
-            waveIsClose = false;
-            //ambientBackgroundMusic.audioSource.UnPause();
-            waveApproachingMusic.audioSource.volume = 0;
-        }
-
-
     }
 
     private void FixedUpdate()
