2905cadee980ae8a7d0b7e791dabc1651276b812
thedudxo <isaacboon@gmail.com>
2020-09-25 08:21:45 +1200

    happy enough

---
+++ Assets/Scripts/Main Menu/ScrollNoStretch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollNoStretch : MonoBehaviour
+{
+
+    [SerializeField] ScrollRect scrollRect;
+    [SerializeField] Scrollbar scrollbar;
+
+    public void OnScroll()
+    {
+        scrollRect.verticalNormalizedPosition = scrollbar.value;
+    }
+
+    public void OnDrag()
+    {
+        scrollbar.value = scrollRect.verticalNormalizedPosition;
+    }
+}
