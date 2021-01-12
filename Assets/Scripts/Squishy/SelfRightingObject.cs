808af577e3f883cc2c2fdd4c91c8b8d364bb7b10
thedudxo <isaacboon@gmail.com>
2021-01-13 07:40:01 +1300

    tidying

--- Assets/Scripts/Squishy/SelfRightingObject.cs
+++ Assets/Scripts/Squishy/SelfRightingObject.cs
@@ -22,6 +22,7 @@ public class SelfRightingObject : MonoBehaviour
     const float pi = Mathf.PI;
 
     float currentRotation;
+    float currentRadians;
     float radsToUpright;
     float desiredDirection;
     float angularMomentum;
@@ -34,7 +35,6 @@ public class SelfRightingObject : MonoBehaviour
     {
         CalculateVariables();
 
-        DecideAppliedTorque();
         DecideIfUpright();
 
         if (!upright)
@@ -49,77 +49,53 @@ public class SelfRightingObject : MonoBehaviour
         }
     }
 
-    private float TimeUntillUpright()
+    private void CalculateVariables()
     {
-        //v  = d/t      vt = d      t  = d/v
-        float angularVelocityRads = rb.angularVelocity * Mathf.Deg2Rad;
-        float time = radsToUpright / angularVelocityRads;
+        currentRotation = rb.transform.rotation.eulerAngles.z % 360;
+        currentRadians = (rb.transform.rotation.eulerAngles.z % 360) * Mathf.Deg2Rad;
 
-        bool movingTowardsUpright = !((radsToUpright >= 0) ^ (angularVelocityRads >= 0));
+        RadsToUpright();
+        DesiredDirection();
+        angularMomentum                  = rb.mass * rb.angularVelocity * Mathf.Deg2Rad;
+        timeToStop                       = Mathf.Abs(angularMomentum / maxTourqe);
+        TourqeToUprightNow();
+        appliedTorque                    = Mathf.Min(torqueToUprightNow, maxTourqe);
+    }
 
-        if (movingTowardsUpright) return time;
+    private void RadsToUpright()
+    {
+        bool onLeftSide = currentRadians < pi;
+
+        if (onLeftSide)
+            radsToUpright = uprightRads - (currentRotation * Mathf.Deg2Rad);
         else
-        {
-            float timeForFullRotation = 2 * pi / Mathf.Abs(angularVelocityRads);
-            return timeForFullRotation - Mathf.Abs(time);
-        }
+            radsToUpright = uprightRads - (-(180 - (currentRotation % 180)) * Mathf.Deg2Rad);
     }
 
-    private void FindRadsToUpright()
+    private void DesiredDirection()
     {
-        if (currentRotation < 180) radsToUpright = uprightRads - (currentRotation * Mathf.Deg2Rad);
-        else radsToUpright = uprightRads - (-(180 - (currentRotation % 180)) * Mathf.Deg2Rad);
-    }
-
-    private void FindDesiredDirection()
-    {
-        if (currentRotation < 180) desiredDirection = 1;
+        if (currentRadians < pi) desiredDirection = 1;
         else desiredDirection = -1;
     }
 
-    private void FindAngularMomentum()
-    {
-        angularMomentum = rb.mass * rb.angularVelocity * Mathf.Deg2Rad;
-    }
 
-    private void FindTimeToStop()
-    {
-        timeToStop = Mathf.Abs(angularMomentum / maxTourqe);
-    }
-
-    private void FindTourqeToUprightNow()
+    private void TourqeToUprightNow()
     {
         float fixedTimeStepSqr = Mathf.Pow(Time.fixedDeltaTime, 2);
         float acceleration = radsToUpright / fixedTimeStepSqr;
         torqueToUprightNow = Mathf.Abs(rb.mass * acceleration);
     }
 
-    private void DecideAppliedTorque()
-    {
-        appliedTorque = Mathf.Min(torqueToUprightNow, maxTourqe);
-    }
 
     private void DecideIfUpright()
     {
         const float tolerance = 0.1f;
 
-        bool withinUpperTolerance = currentRotation * Mathf.Deg2Rad < uprightRads + tolerance;
-        bool withinLowerTolerance = currentRotation * Mathf.Deg2Rad > uprightRads - tolerance;
+        bool withinUpperTolerance = currentRadians < uprightRads + tolerance;
+        bool withinLowerTolerance = currentRadians > uprightRads - tolerance;
 
         upright = withinUpperTolerance && withinLowerTolerance;
 
-    }
-
-    private void CalculateVariables()
-    {
-        currentRotation = rb.transform.rotation.eulerAngles.z % 360;
-        //float currentRotationRads = (rb.transform.rotation.eulerAngles.z % 360) * Mathf.Deg2Rad;
-
-        FindRadsToUpright();
-        FindDesiredDirection();
-        FindAngularMomentum();
-        FindTimeToStop();
-        FindTourqeToUprightNow();
     }
 
     private void SpeedUp()
@@ -142,4 +118,19 @@ public class SelfRightingObject : MonoBehaviour
     bool Moving => rb.angularVelocity > 1;
     bool NotGoingToOvershoot => (TimeUntillUpright() <= timeToStop);
 
+    private float TimeUntillUpright()
+    {
+        //v  = d/t      vt = d      t  = d/v
+        float angularVelocityRads = rb.angularVelocity * Mathf.Deg2Rad;
+        float time = radsToUpright / angularVelocityRads;
+
+        bool movingTowardsUpright = !((radsToUpright >= 0) ^ (angularVelocityRads >= 0));
+
+        if (movingTowardsUpright) return time;
+        else
+        {
+            float timeForFullRotation = 2 * pi / Mathf.Abs(angularVelocityRads);
+            return timeForFullRotation - Mathf.Abs(time);
+        }
+    }
 }
