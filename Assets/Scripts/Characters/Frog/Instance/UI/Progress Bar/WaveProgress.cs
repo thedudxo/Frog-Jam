c61456dbf0763b9abb53b1cf432f9b3f3a3e287a
thedudxo <isaacboon@gmail.com>
2021-03-10 21:06:20 +1300

    Progress bar utilities attached wave, works separately for each player

--- Assets/Scripts/Characters/Frog/Instance/UI/Progress Bar/WaveProgress.cs
+++ Assets/Scripts/Characters/Frog/Instance/UI/Progress Bar/WaveProgress.cs
@@ -1,22 +1,27 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace FrogScripts.Progress
 {
     public class WaveProgress : MonoBehaviour, IProgressTracker
     {
-        [SerializeField] FrogProgress frogProgress;
-        WaveFrogMediatior waveMediator;
-
-        private void Start()
-        {
-            waveMediator = frogProgress.frog.currentLevel.waveFrogMediatior;
-        }
+        [SerializeField] Frog frog;
+        [SerializeField] FrogWaveInteractions waveInteractions;
+        [SerializeField] Slider waveProgressBar;
 
         public void UpdateProgress()
         {
-            //float wavePosX = waveTransform.position.x;
-            //waveProgressBar.value = (wavePosX - level.startLength) / (level.end - level.startLength);
+            waveScripts.Wave wave = waveInteractions.attachedWave;
+            if (wave == null || wave.state == waveScripts.Wave.State.inactive)
+            {
+                waveProgressBar.value = 0;
+                return;
+            }
+
+            float wavePosX = wave.transform.position.x;
+            LevelScripts.Level level = frog.currentLevel;
+            waveProgressBar.value = (wavePosX - level.startLength) / (level.end - level.startLength);
         }
     }
 }
