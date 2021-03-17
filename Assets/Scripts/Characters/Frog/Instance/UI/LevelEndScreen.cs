7b5cb9d454bb9d07b7497322438bedd317b01362
thedudxo <isaacboon@gmail.com>
2021-03-18 10:54:59 +1300

    Endscreen suggests correct restart key

--- Assets/Scripts/Characters/Frog/Instance/UI/LevelEndScreen.cs
+++ Assets/Scripts/Characters/Frog/Instance/UI/LevelEndScreen.cs
@@ -5,7 +5,8 @@ namespace FrogScripts
 {
     public class LevelEndScreen : MonoBehaviour
     {
-        [SerializeField] Text timeTaken, pbTimeTaken, splitsSum;
+        [SerializeField] Frog frog;
+        [SerializeField] Text timeTaken, pbTimeTaken, splitsSum, restartPrompt;
         [SerializeField] GameObject endScreen;
 
         const string timeFormat = "F3";
@@ -14,10 +15,12 @@ namespace FrogScripts
         {
             if (splitsSum > 1000000)
                 Debug.Log("bug");
+
             endScreen.SetActive(true);
             timeTaken.text = time.ToString(timeFormat);
             pbTimeTaken.text = pbTime.ToString(timeFormat);
             this.splitsSum.text = splitsSum.ToString(timeFormat);
+            restartPrompt.text = $"Press {frog.controlls.suicideKey} to restart";
         }
 
         public void Disable()
