98fdfc1ee96504866985e845b2dfc6a0bae202e2
thedudxo <isaacboon@gmail.com>
2021-04-11 13:29:43 +1200

    removed conflict marker

--- Assets/Scripts/Characters/Frog/Instance/UI/LevelEndScreen.cs
+++ Assets/Scripts/Characters/Frog/Instance/UI/LevelEndScreen.cs
@@ -15,17 +15,11 @@ namespace FrogScripts
 
         public void Enable(float time, float pbTime, float splitsSum)
         {
-<<<<<<< Updated upstream
             if (splitsSum > 10000000)
             {
                 bugOccured = true;
                 Debug.LogError("That annoying split bug");
             }
-                
-=======
-            if (splitsSum > 1000000)
-                Debug.LogError("That annoying split bug");
->>>>>>> Stashed changes
 
             endScreen.SetActive(true);
             timeTaken.text = time.ToString(timeFormat);
