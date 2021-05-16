7948cd860af1596632d4be80c512580d1bbb4394
thedudxo <isaacboon@gmail.com>
2021-05-16 13:19:57 +1200

    Jump input wrapper PC

--- Assets/Scripts/Characters/Constructs/Frogs/Instances/Controllers/Inputs/Jump.cs
+++ Assets/Scripts/Characters/Constructs/Frogs/Instances/Controllers/Inputs/Jump.cs
@@ -1,9 +1,57 @@
 ﻿using UnityEngine;
+using Frogs.Instances.Jump;
 
 namespace Frogs.Instances.Inputs
 {
-    public class Jump : MonoBehaviour
+    public class Jump : MonoBehaviour, INotifyOnAnyRespawn
     {
-        [SerializeField] public KeyCode key = KeyCode.Space;
+        [SerializeField] Frog frog;
+        [SerializeField] public KeyCode key;
+        [SerializeField] JumpController jumpController;
+        float chargeTime = 0;
+
+        private void Start()
+        {
+            frog.events.SubscribeOnAnyRespawn(this);
+        }
+
+        private void Update()
+        {
+#if UNITY_ANDROID
+#else
+
+            if (Input.GetKey(key))
+            {
+                HoldingInput();
+            }
+
+            if (Input.GetKeyUp(key))
+            {
+                ReleasedInput();
+            }
+#endif
+        }
+
+        private void ReleasedInput()
+        {
+            chargeTime = 0;
+            jumpController.AttemptJump();
+            jumpController.SetJumpCharge(0);
+        }
+
+        private void HoldingInput()
+        {
+            chargeTime += Time.deltaTime;
+            jumpController.SetJumpCharge(chargeTime);
+        }
+
+        public void OnAnyRespawn()
+        {
+            chargeTime = 0;
+        }
+
+
+
+
     }
 }
