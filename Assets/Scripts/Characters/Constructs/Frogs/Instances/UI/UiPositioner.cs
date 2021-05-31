f0ecb92a9b52b202b99a27563dffca3273f558f8
thedudxo <isaacboon@gmail.com>
2021-06-01 09:49:49 +1200

    Dynamic UI Scaling (setup, needs hooking up)

---
+++ Assets/Scripts/Characters/Constructs/Frogs/Instances/UI/UiPositioner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Frogs.Instances.UI
+{
+    class UiPositioner : MonoBehaviour
+    {
+        [SerializeField] RectTransform mainUIPanel;
+        [SerializeField] CanvasScaler scaler;
+
+#if UNITY_ANDROID
+        private void Awake()
+        {
+            float scale = 2;
+            scaler.scaleFactor = scale;
+
+            //if singleplayer
+            AdjustUISizeToFillScreen(scale);
+        }
+#endif
+
+        /// <summary> main UI panel is normaly half the screen size </summary>
+        void AdjustUISizeToFillScreen(float scale)
+        {
+            float offset = (Screen.height / 2) / scale;
+            mainUIPanel.offsetMax = new Vector2(mainUIPanel.offsetMax.x, offset);
+            mainUIPanel.offsetMin = new Vector2(mainUIPanel.offsetMin.x, -offset);
+        }
+    }
+}
