e835c73b3fdd0f492ec42da7a8c8d03df8fe93bc
thedudxo <isaacboon@gmail.com>
2021-07-06 11:24:06 +1200

    Menu updates

---
+++ Assets/Scripts/Main Menu/MenuSetup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Menus
+{
+    public class MenuSetup : MonoBehaviour
+    {
+        [SerializeField] GameObject PCMainMenuButtons;
+        [SerializeField] GameObject MobileMainMenuButtons;
+
+        private void Awake()
+        {
+            switch (GM.platform)
+            {
+                case GM.Platform.PC:
+                    PCMainMenuButtons.SetActive(true);
+                    MobileMainMenuButtons.SetActive(false);
+                    break;
+
+                case GM.Platform.Android:
+                    MobileMainMenuButtons.SetActive(true);
+                    PCMainMenuButtons.SetActive(false);
+                    break;
+            }
+        }
+    }
+}
